161797195b063bc1567276d10f39b5a3efda42ab
GalBenor <benorgal@gmail.com>
2016-04-05 19:30:42 +0300

    task2

--- mkfs.c
+++ mkfs.c
@@ -1,3 +1,4 @@
+//mkfs
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -11,7 +12,7 @@
 #include "stat.h"
 #include "param.h"
 
-#define static_assert(a, b) do { switch (0) case 0: case (a): ; } while (0)
+//#define static_assert(a, b) do { switch (0) case 0: case (a): ; } while (0)
 
 int nblocks = (995-LOGSIZE);
 int nlog = LOGSIZE;
